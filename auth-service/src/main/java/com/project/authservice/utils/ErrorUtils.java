f8665f21968087032aee5696249d2f213bf206b2
aditisharma988 <aditisharma140802@gmail.com>
2026-06-21 21:25:43 +0530

    refactor: utilize native spring messagesource interface to remove redundant bean declaration

--- auth-service/src/main/java/com/project/authservice/utils/ErrorUtils.java
+++ auth-service/src/main/java/com/project/authservice/utils/ErrorUtils.java
@@ -4,8 +4,8 @@ import com.project.authservice.exception.ValidationError;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.Nullable;
+import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -16,7 +16,7 @@ import java.util.Locale;
 @RequiredArgsConstructor
 public class ErrorUtils {
 
-    private final ResourceBundleMessageSource source;
+    private final MessageSource source;
 
     public ValidationError getError(String fieldName, String fieldValue, String messageKey) {
         String errorMessage = source.getMessage(messageKey, new Object[]{}, getLocale());
