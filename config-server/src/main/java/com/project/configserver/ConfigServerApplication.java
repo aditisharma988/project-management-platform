3e1d40756f33a241d9c88f629bafebc954b87298
aditisharma988 <aditisharma140802@gmail.com>
2026-06-22 12:50:49 +0530

    Refactored main application file name.

--- config-server/src/main/java/com/project/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/project/configserver/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.project.config_server;
+package com.project.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- config-server/src/test/java/com/project/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/project/configserver/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.project.config_server;
+package com.project.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
